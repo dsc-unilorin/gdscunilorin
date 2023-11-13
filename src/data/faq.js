36f6b0ed75ce591bf2872d1eeb858d2dce5319d0
LmOpe <lawalmuhammedope@gmail.com>
2023-11-13 23:09:50 +0100

    Add Faq

---
+++ src/data/faq.js
@@ -0,0 +1,31 @@
+export const faqs = [
+    {
+        "id": 1,
+        "question": "What is GDSC?",
+        "answer": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus consequatur nulla quae enim quibusdam. Fugiat inventore facilis laborum at, eaque accusantium in beatae dignissimos aliquam ipsam non quod voluptatibus quaerat commodi illo modi voluptatum eius, eos, ab maxime laudantium veritatis"
+    },
+
+    {
+        "id": 2,
+        "question": "What is GDSC?",
+        "answer": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus consequatur nulla quae enim quibusdam. Fugiat inventore facilis laborum at, eaque accusantium in beatae dignissimos aliquam ipsam non quod voluptatibus quaerat commodi illo modi voluptatum eius, eos, ab maxime laudantium veritatis"
+    },
+
+    {
+        "id": 3,
+        "question": "What is GDSC?",
+        "answer": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus consequatur nulla quae enim quibusdam. Fugiat inventore facilis laborum at, eaque accusantium in beatae dignissimos aliquam ipsam non quod voluptatibus quaerat commodi illo modi voluptatum eius, eos, ab maxime laudantium veritatis"
+    },
+
+    {
+        "id": 4,
+        "question": "What is GDSC?",
+        "answer": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus consequatur nulla quae enim quibusdam. Fugiat inventore facilis laborum at, eaque accusantium in beatae dignissimos aliquam ipsam non quod voluptatibus quaerat commodi illo modi voluptatum eius, eos, ab maxime laudantium veritatis"
+    },
+
+    {
+        "id": 5,
+        "question": "What is GDSC?",
+        "answer": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus consequatur nulla quae enim quibusdam. Fugiat inventore facilis laborum at, eaque accusantium in beatae dignissimos aliquam ipsam non quod voluptatibus quaerat commodi illo modi voluptatum eius, eos, ab maxime laudantium veritatis"
+    }
+]
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,14 @@
-import { Box } from '@chakra-ui/react';
-import Layout from '../components/base/Layout';
-import { CoreTeam } from '../components/sections/home/CoreTeam';
-function Homepage ()
-{
-
+import { Box } from "@chakra-ui/react";
+import Layout from "../components/base/Layout";
+import { CoreTeam } from "../components/sections/home/CoreTeam";
+import { FAQ } from "../components/sections/home/Faq";
+function Homepage() {
   return (
     <>
       <Layout>
-        <Box p={ 4 }>Main Content Here</Box>
+        <Box p={4}>Main Content Here</Box>
         <CoreTeam />
+        <FAQ />
       </Layout>
     </>
   );
